c460534734c03b816cb881816ab3cdac222fe5e6
Pulkit Gupta <pulkonet@gmail.com>
2017-09-12 00:45:02 +0530

    obstacles added

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
-
+var myObstacles = [];
 
 function startGame() {
     myGameArea.start();
@@ -25,6 +25,24 @@ function component(width, height, color, x, y) {
         this.x += this.speedX;
         this.y += this.speedY;
     }
+    this.crashWith = function (otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+            (mytop > otherbottom) ||
+            (myright < otherleft) ||
+            (myleft > otherright)) {
+            crash = false;
+        }
+        return crash;
+    }
 }
 
 var keyPressed = {}
@@ -36,6 +54,8 @@ var myGameArea = {
         this.canvas.height = 800;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;
+        this.acc = -1;
         this.interval = setInterval(updateGameArea, 10);
         window.addEventListener('keydown', function (e) {
             keyPressed[e.keyCode] = true;
@@ -47,11 +67,37 @@ var myGameArea = {
 
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop: function () {
+        clearInterval(this.interval);
     }
 }
 
+function everyinterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) { return true; }
+    return false;
+}
+
 function updateGameArea() {
+    var x, y;
+    for (i = 0; i < myObstacles.length; i += 1) {
+        if (gpLeft.crashWith(myObstacles[i]) || gpRight.crashWith(myObstacles[i])) {
+            myGameArea.stop();
+            return;
+        }
+    }
     myGameArea.clear();
+    myGameArea.acc += 1;
+    myGameArea.frameNo += 1;    
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+        x = 150;
+        y = -65;
+        myObstacles.push(new component(100, 65, "yellow", x, y));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+        myObstacles[i].y += 1;
+        myObstacles[i].update();
+    }
     gpLeft.speedX = 0;
     gpRight.speedX = 0;
     if (keyPressed["39"] && keyPressed["37"]) {
@@ -82,7 +128,6 @@ function updateGameArea() {
         gpLeft.x = 160;
         gpRight.x = 200;
     }
-
     gpLeft.newPos();
     gpLeft.update();
     gpRight.newPos();
